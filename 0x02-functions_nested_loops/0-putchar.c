0e3e9b80ec4885655937994e2fa90d43decb9c93
Aime-Pierre <blessedchildap@gmail.com>
2022-06-14 16:22:01 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * Description: prints 'Holberton \n'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char str[] = "_puchar\n";
+int i = 0;
+
+while (str[i] != '\0')
+	{
+	char c = str[i];
+
+	_putchar(c);
+	i++;
+	}
+return (0);
+}
